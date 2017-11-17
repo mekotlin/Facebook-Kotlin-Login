07992cc0b48155bcd8d65a431a7109169c247343
Dnyaneshwar Dalvi <dnyaneshwar@onevoicetransmedia.com>
2017-11-17 17:54:02 +0530

    Initial Commit

---
+++ app/src/main/java/mekotlinapps/dnyaneshwar/in/facebooklogin/MainActivity.kt
@@ -0,0 +1,43 @@
+package mekotlinapps.dnyaneshwar.`in`.facebooklogin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val callbackManager = CallbackManager.Factory.create()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onError(error: FacebookException?) {
+
+            }
+
+            override fun onSuccess(result: LoginResult?) {
+
+            }
+
+            override fun onCancel() {
+
+            }
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+}
