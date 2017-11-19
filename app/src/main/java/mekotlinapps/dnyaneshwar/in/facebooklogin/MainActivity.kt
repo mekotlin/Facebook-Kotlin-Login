f6ed48fd8a58f0af49d41263b088a91c2bdc1f56
dnyaneshwar <dalvi.dnyane@gmail.com>
2017-11-19 17:27:08 +0530

    file removed

--- app/src/main/java/mekotlinapps/dnyaneshwar/in/facebooklogin/MainActivity.kt
+++ app/src/main/java/mekotlinapps/dnyaneshwar/in/facebooklogin/MainActivity.kt
@@ -1,13 +1,17 @@
 package mekotlinapps.dnyaneshwar.`in`.facebooklogin
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import android.util.Base64
+import android.util.Log
+import android.view.View
+import com.facebook.*
 import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,26 +20,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-            override fun onError(error: FacebookException?) {
 
+        loginButton.setReadPermissions("public_profile")
+
+        generateHashKey()
+
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onError(error: FacebookException?) {
             }
 
             override fun onSuccess(result: LoginResult?) {
-
+                val profile = Profile.getCurrentProfile()
+                val name = profile.name
+                tvMsg.setText("Thank you $name")
+                toast("Thank you $name")
             }
 
             override fun onCancel() {
-
             }
         })
+    }
 
+
+    fun generateHashKey() {
+        val info = packageManager.getPackageInfo("mekotlinapps.dnyaneshwar.in.facebooklogin", PackageManager.GET_SIGNATURES)
+
+        for (signature in info.signatures) {
+            val md = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.e("HashKey :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data);
-
-
     }
 }
